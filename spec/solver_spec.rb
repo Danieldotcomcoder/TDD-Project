9848804be9c1371a307282393be54a49b0fcebae
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-02-03 16:15:26 +0200

    create solver class

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,22 +1,23 @@
-require './solver.rb'
+require_relative '../solver'
 
 describe Solver do
-    context " Test the Solver class"
+    solver = Solver.new()
+    context " Test the Solver class" 
     it "test the factorial method" do
-        expect(Solver.factorial(4)).to eq 24
-        expect(Solver.factorial(5)).to eq 120
+        expect(solver.factorial(4)).to eq 24
+        expect(solver.factorial(5)).to eq 120
     end
 
 
     it "test the reverse method" do 
-        expect(Solver.reverse('Danny')).to eq 'ynnaD'
-        expect(Solver.reverse('steven')).to eq 'nevets'
+        expect(solver.reverse('Danny')).to eq 'ynnaD'
+        expect(solver.reverse('steven')).to eq 'nevets'
     end
 
     it "test the fizzbuzz method" do
-        expect(Solver.fizzbuzz(3)).to eq  'fizz'
-        expect(Solver.fizzbuzz(5)).to eq  'buzz'
-        expect(Solver.fizzbuzz(15)).to eq 'fizzbuzz'
-        expect(Solver.fizzbuzz(10)).to eq '10'
+        expect(solver.fizzbuzz(3)).to eq  'fizz'
+        expect(solver.fizzbuzz(5)).to eq  'buzz'
+        expect(solver.fizzbuzz(15)).to eq 'fizzbuzz'
+        expect(solver.fizzbuzz(11)).to eq '11'
     end
 end
